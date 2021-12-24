5e282199177af4393e42151e27d937e4fc912d94
sKadirbek <200103020@stu.sdu.edu.kz>
2021-12-24 11:43:46 +0600

    add prgrams page

--- js/main.js
+++ js/main.js
@@ -1,47 +1,4 @@
 $(document).ready(function(){
-    // $('a[href^="#form"]').click(function(){
-    //     var target = $(this).attr('href');
-    //     $('html, body').animate({scrollTop: $(target).offset().top}, 800);
-    //     return false;
-    // });
-
-    // $('a[href^="#about_me"]').click(function(){
-    //     var target = $(this).attr('href');
-    //     $('html, body').animate({scrollTop: $(target).offset().top}, 800);
-    //     return false;
-    // });
-
-    // $('a[href^="#porfolio"]').click(function(){
-    //     var target = $(this).attr('href');
-    //     $('html, body').animate({scrollTop: $(target).offset().top}, 800);
-    //     return false;
-    // });
-    
-    // $('a[href^="#feedbacks"]').click(function(){
-    //     var target = $(this).attr('href');
-    //     $('html, body').animate({scrollTop: $(target).offset().top}, 800);
-    //     return false;
-    // });
-
-    // $('a[href^="#header"]').click(function(){
-    //     var target = $(this).attr('href');
-    //     $('html, body').animate({scrollTop: $(target).offset().top}, 800);
-    //     return false;
-    // });
-    
- 
-    // $(window).scroll(function(){
-    //     if ($(this).scrollTop() > 100) {
-    //     $('.arrow').fadeIn();
-    //     } else {
-    //     $('.arrow').fadeOut();
-    //     }
-    //     });
-         
-    //     $('.scrollup').click(function(){
-    //     $("html, body").animate({ scrollTop: 0 }, 800);
-    //     return false;
-    // });
     
     $('.banner_slider').slick({
         slidesToShow: 1,
@@ -63,21 +20,6 @@ $(document).ready(function(){
         cssEase: 'ease',
     });
 
-    // $('a.btn_form').click( function(event){
-    //     event.preventDefault();
-    //     $('#myOverlay').fadeIn(297,	function(){
-    //       $('#myModal') 
-    //       .css('display', 'block')
-    //       .animate({opacity: 1}, 198);
-    //     });
-    //   });
-    
-    //   $('#myModal__close, #myOverlay').click( function(){
-    //     $('#myModal').animate({opacity: 0}, 198, function(){
-    //       $(this).css('display', 'none');
-    //       $('#myOverlay').fadeOut(297);
-    //     });
-    //   });
     $(function(){	
         $('.news_desc').each(function(){
             var column = 0;
